27e4d1ed63716f54611d437a540632445b49a44c
Soare Catalin <stevensuncatalin369@gmail.com>
2022-10-05 08:07:49 +0300

    the subjects carousel component looks decent

--- src/routes/teacher/Grade.svelte
+++ src/routes/teacher/Grade.svelte
@@ -5,14 +5,29 @@
   import {fetchStudents} from '../../fetch/fetch';
   import { writable } from 'svelte/store'
 
+  // kiui
   import Subjects from '../../kiui/Subjects.svelte'
   import Student from '../../kiui/Student.svelte'
+  import HeaderBack from '../../kiui/HeaderBack.svelte'
+    import Grade from '../../kiui/Grade.svelte'
 
   export let params = {}
 
   let currentSubject = writable("");
 
+  let roman = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII"]
+
 </script>
+
+<HeaderBack />
+
+
+{#if $currentSubject}
+  <div id="heading">
+    Clasa a {roman[$subjects[params.gradeKey][$currentSubject].grade.gradeNumber]}-a
+    {$subjects[params.gradeKey][$currentSubject].grade.gradeLetter.toUpperCase()}
+  </div>
+{/if}
 
 <Subjects subjects={$subjects} gradeKey={params.gradeKey} {currentSubject} />
 
@@ -21,3 +36,15 @@
     <Student gradeKey={params.gradeKey} {currentSubject} {student} />
   {/each}
 {/await}
+
+<style scoped>
+	#heading {
+		font-size: 2em;
+		color: var(--black);
+		margin-left: 18px;
+		margin-top: 10px;
+		margin-bottom: 5px;
+		font-weight: 600;
+		font-family: sans-serif;
+	}
+</style>
